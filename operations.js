148db1469162d210ecdcef5f21f029046095fb85
fa4favourite <mountnina@gmail.com>
2023-08-01 21:25:53 +0100

    Tried problem solving skill in JS

---
+++ operations.js
@@ -0,0 +1,23 @@
+var x = 2;
+var y = 3;
+var twice = (x + y) * 2 ;
+console.log(twice);
+
+
+var a = 4;
+var x = a % 10;
+var c = a + x;
+console.log(c);
+
+var n = 4;
+var x = 10;
+var b = x^n;
+console.log(b);
+
+var a = 15;
+var x = a + 10;
+var y = a - 10;
+var z = a % 10;
+var b = x*y*z;
+var answer = a/b;
+console.log(answer);
